bb36a7dc47d52c56ae1d89c29e293ffb9a7fcafe
Tam Nguyen <ntam741456@gmail.com>
2025-09-12 21:19:00 +0700

    Pushing Models Api

---
+++ ClinicManagement_API/Models/Supplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClinicManagement_API.Models;
+
+public partial class Supplier
+{
+    public int SupplierId { get; set; }
+
+    public string SupplierName { get; set; } = null!;
+
+    public string? ContactEmail { get; set; }
+
+    public string? ContactPhone { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<ImportBill> ImportBills { get; set; } = new List<ImportBill>();
+}
