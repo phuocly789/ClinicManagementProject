795d8758f115948abbd58a4980650e7c0f57dc62
phuocly789 <phuoclyminh789@gmai.com>
2025-11-01 07:58:48 +0700

    fix forder

--- ClinicManagement_WebAppOld/Models/Admin/PageResult.cs
+++
@@ -1,7 +0,0 @@
-public class PagedResult<T>
-{
-    public int TotalItems { get; set; }
-    public int Page { get; set; }
-    public int PageSize { get; set; }
-    public List<T> Items { get; set; }
-}
--- ClinicManagement_WebAppOld/Models/Admin/ServiceDTO.cs
+++
@@ -1,8 +0,0 @@
-public class ServiceDTO
-{
-    public int ServiceId { get; set; }
-    public string ServiceName { get; set; } = string.Empty;
-    public string ServiceType { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public string? Description { get; set; }
-}
